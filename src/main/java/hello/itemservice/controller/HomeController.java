77ddd967e57c99c14f7d23b028ea2f58564b8795
PeachCoolPiece <rlglxh@naver.com>
2024-04-15 17:12:41 +0900

    redis

---
+++ src/main/java/hello/itemservice/controller/HomeController.java
@@ -0,0 +1,19 @@
+package hello.itemservice.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home(HttpSession session) {
+     
+        session.setAttribute("username", "user123");
+        return "basic/index";
+    }
+    
+}
---
+++ src/main/java/hello/redis/SessionConfig.java
@@ -0,0 +1,13 @@
+package hello.redis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+
+@Configuration
+@EnableRedisHttpSession
+public class SessionConfig {
+    
+
+}
