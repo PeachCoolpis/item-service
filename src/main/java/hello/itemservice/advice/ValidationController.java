64cdc8d179e3322a0fc4e3d5efc0eb92e2064f4a
PeachCoolPiece <rlglxh@naver.com>
2024-05-02 12:58:12 +0900

    설정변경

---
+++ src/main/java/hello/itemservice/advice/ValidationController.java
@@ -0,0 +1,14 @@
+//package hello.itemservice.advice;
+//
+//
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@ControllerAdvice(basePackages = "hello.itemservice.controller")
+//public class ValidationController {
+//
+//    @ExceptionHandler(Exception.class)
+//    public String exceptionHandler() {
+//        return null;
+//    }
+//}
